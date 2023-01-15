e3985140f9a634b6807249b8c9540dc3ecaca984
ionachadwick97 <ionachadwick97@gmail.com>
2023-01-15 21:08:37 +0000

    added prompts, generated random password, needs link between prompt input adn generation

--- starter/script.js
+++ starter/script.js
@@ -1,3 +1,17 @@
+// Generate a password when the button is clicked
+// Present a series of prompts for password criteria
+// Length of password
+// At least 10 characters but no more than 64.
+// Character types
+// Lowercase
+// Uppercase
+// Numeric
+// Special characters ($@%&*, etc)
+// Code should validate for each input and at least one character type should be selected
+// Once prompts are answered then the password should be generated and displayed in an alert or written to the page
+
+
+
 // Array of special characters to be included in password
 var specialCharacters = [
   '@',
@@ -88,20 +102,51 @@ var upperCasedCharacters = [
   'Z'
 ];
 
+var arrays = [specialCharacters, upperCasedCharacters, lowerCasedCharacters, numericCharacters];
+
+var reqPassLength = 10;
+
 // Function to prompt user for password options
 function getPasswordOptions() {
-
+  var OK = false
+do{
+  reqPassLength = prompt('What length do you require?') 
+  if(reqPass2Length > 9 && reqPassLength < 65) { OK = true; }
+} while (OK === false);
 }
+
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+//get random elements from an array
+return arr[Math.floor(Math.random()*arr.length)];
 }
+
 
 // Function to generate password with user input
-function generatePassword() {
 
+function generatePassword() {
+  let passText = "";
+  let matched = [0, 0, 0, 0];
+  let validated = false;
+  let i = 0;
+  while(i < 64) {
+    let arrayNumber = Math.floor(Math.random()*arrays.length);
+    matched[arrayNumber] = 1;
+    let temp1 = passText;
+    let temp2 = getRandom(arrays[arrayNumber]);
+    passText = temp1.concat(temp2);
+    if(matched[0] === 1 && matched[1] === 1 && matched[2] === 1 && matched[3] === 1) { validated = true; }
+    i++;
+
+     if (i > reqPassLength && validated === true) break;
+  }
+ 
+  return passText;
 }
+
+getPasswordOptions()
+// while ()
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
@@ -116,3 +161,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
+
